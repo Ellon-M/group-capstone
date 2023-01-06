bdcb2c890471ebc2eb9f4c3b98c17dae13deb74a
ellon <mordecaimwema11@gmail.com>
2023-01-06 18:29:10 +0300

    remove all files

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  moduleFileExtensions: ['js', 'jsx', 'json'],
-  transformIgnorePatterns: ['<rootDir>/node_modules/'],
-  testEnvironment: 'jsdom',
-  moduleNameMapper: {
-    '\\.(css|sass)$': 'identity-obj-proxy',
-  },
-};
